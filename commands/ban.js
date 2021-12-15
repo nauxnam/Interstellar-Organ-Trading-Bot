55a9dfeac0bd2e5bce3e619b360a4687e397a18f
nauxnam <hsnnbq@gmail.com>
2021-12-15 20:22:17 +0300

    ban and kick permissions added.
    Only members with Ban and Kick permissions can use the command. Also pump and dump files has been created. It will be modified in future.

--- commands/ban.js
+++ commands/ban.js
@@ -2,13 +2,17 @@ module.exports = {
     name: "ban",
     description: "Ban user command.",
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarget = message.guild.members.cache.get(memeber.id);
-            memberTarget.ban();
-            message.channel.send("User has been banned.");
-        }else{
-            messsage.channel.send('You couldn`t ban that member.');
+        if(message.member.permissions.has("BAN_MEMBERS")){
+            const member = message.mentions.users.first();
+            if(member){
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberTarget.ban();
+                message.channel.send("User has been banned.");
+            }else{
+                messsage.channel.send('You couldn`t ban that member.');
+            }
+        } else {
+            message.channel.send('You don`t have enough permissions.')
         }
     }
 }
--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,17 @@ module.exports = {
     name: "kick",
     description: "Kick user command.",
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarget = message.guild.members.cache.get(memeber.id);
-            memberTarget.kick();
-            message.channel.send("User has been kicked.");
-        }else{
-            message.channel.send('You couldn`t kick that member.');
+        if(message.member.permissions.has("KICK_MEMBERS")){
+            const member = message.mentions.users.first();
+            if(member){
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberTarget.kick();
+                message.channel.send("User has been kicked.");
+            }else{
+                message.channel.send('You couldn`t kick that member.');
+            }
+        } else {
+            message.channel.send('You don`t have enough permissions to execute this command.');
         }
     }
 }
